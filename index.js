6d2ef8abf7e80c41e7205e658ba5c91c2247cf04
Ephraim Bundi <ebundi04@gmail.com>
2021-07-25 16:32:03 +0300

    modied about us section

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".img1").click(function(){
+      $(".show1").slideToggle(1500);
+    $(".hide1").slideToggle(1500);
+    }); 
+  });
+  
+  $(document).ready(function(){
+    $(".img2").click(function(){
+       $(".hide2").slideToggle(1500);
+    $(".show2").slideToggle(1500);
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".img3").click(function(){
+       $(".hide3").slideToggle(1500);
+    $(".show3").slideToggle(1500);
+    });
+  });
+  
+  
+  
+ 
